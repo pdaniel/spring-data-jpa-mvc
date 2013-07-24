06f8b8be9f28af3f535a571a8bf0b34966f612a1
toptoolz <posirca.daniel@gmail.com>
2013-07-25 00:26:14 +0300

    Abstract classes

---
+++ api/src/main/java/com/ondevel/api/repository/AbstractRepository.java
@@ -0,0 +1,9 @@
+package com.ondevel.api.repository;
+
+/**
+ * @author Daniel P.
+ * @version 1.0
+ *          7/25/13
+ */
+public class AbstractRepository {
+}
---
+++ api/src/main/java/com/ondevel/api/service/AbstractService.java
@@ -0,0 +1,9 @@
+package com.ondevel.api.service;
+
+/**
+ * @author Daniel P.
+ * @version 1.0
+ *          7/25/13
+ */
+public class AbstractService {
+}
